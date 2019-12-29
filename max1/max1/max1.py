f9870d5b8b10a99aa6dd0ac12f58501ccbce492b
kb12pl <kb12pl@gmail.com>
2019-12-29 15:26:18 +0100

    123

--- max1/max1/max1.py
+++ max1/max1/max1.py
@@ -1,11 +1,25 @@
-
 from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
+a=120/4
+#rysowanie przedniej nogi
+fd(a); lt(90)
+fd(a); rt(90)
+fd(a); rt(90)
+#rysowanie tylniej nogi
+fd(a); lt(90)
+fd(a); lt(90)
+#rysowanie tyłu
+fd(3*a); lt(90)
+#rysowanie ogona
+fd(a); lt(90)
+fd(a); rt(90)
+#rysowanie grzbietu
+fd(a); rt(90)
+#rysowanie głowy
+fd(2*a); lt(90)
+fd(2*a); lt(90)
+fd(a); lt(90)
+fd(a); rt(90)
+#rysowanie przodu
+fd(3*a); lt(90)
+
 done()
