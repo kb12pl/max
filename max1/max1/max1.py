8c72f15c708851892e68e40666ee65987c7243e2
kb12pl <kb12pl@gmail.com>
2019-12-29 15:29:23 +0100

    123

--- max1/max1/max1.py
+++ max1/max1/max1.py
@@ -1,25 +1,13 @@
 from turtle import *
-a=120/4
-#rysowanie przedniej nogi
-fd(a); lt(90)
-fd(a); rt(90)
-fd(a); rt(90)
-#rysowanie tylniej nogi
-fd(a); lt(90)
-fd(a); lt(90)
-#rysowanie tyłu
-fd(3*a); lt(90)
-#rysowanie ogona
-fd(a); lt(90)
-fd(a); rt(90)
-#rysowanie grzbietu
-fd(a); rt(90)
-#rysowanie głowy
-fd(2*a); lt(90)
-fd(2*a); lt(90)
-fd(a); lt(90)
-fd(a); rt(90)
-#rysowanie przodu
-fd(3*a); lt(90)
+
+
+
+def wielo(n):
+    for i in range(n):
+        fd(100)
+        rt(360/n)
+    
+wielo(5)
+
 
 done()
